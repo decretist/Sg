ca0b8042dcc95ed4cb7d34d6b824136d40a57a63
Paul Evans <decretist@gmail.com>
2019-01-02 22:54:50 -0800

    Add rollup script for hand-edited case statements

---
+++ hand/rollup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Paul Evans
+#  2 January 2018
+# 25 October 2015
+#
+if [ -f Gratian3.txt ];
+then
+    rm -i Gratian3.txt
+fi
+while read -r line
+do
+    file=cases/"Sg $line".txt
+    cat "$file" >> Gratian3.txt
+done < toc_cases.txt
+# shasum -c Gratian3.sha1
